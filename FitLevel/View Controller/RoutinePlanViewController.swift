daa299d3cef01b6b9a956a19d5073c4c2faaf490
Jacky Eng <jeng0001@student.monash.edu>
2020-05-09 03:04:45 +1000

    Created new swift files for each view controller in main storyboard. Created group folders to organise newly added files.

---
+++ FitLevel/View Controller/RoutinePlanViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoutinePlanViewController.swift
+//  FitLevel
+//
+//  Created by Jacky Eng on 09/05/2020.
+//  Copyright © 2020 Jacky Eng. All rights reserved.
+//
+
+import UIKit
+
+class RoutinePlanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
